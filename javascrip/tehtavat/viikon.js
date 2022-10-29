a5311e3a3342f81761a854fd2daf2487f774842e
hasanbadr <hasan.badr@edu.keuda.fi>
2022-10-29 22:01:02 +0300

    tehtävä

--- javascrip/tehtavat/viikon.js
+++ javascrip/tehtavat/viikon.js
@@ -1,84 +1,99 @@
-function kolme()
+function jarjjesta()
 {
-  var luku1, luku2, luku3;
-  luku1 = document.getElementsBy('l1').value;
-  luku2 = document.getElementById('l2').value;
-  luku3 = document.getElementById('l3').value;
-  /*document.getElementById('t1').innerHtml = luku.sort()*/
-  if(luku1 < luku2 && luku1 < luku3)
+  var luku1 =parseunt(document.getElementById('l1').value);
+  var luku2 =parseunt(document.getElementById('l2').value);
+  var luku3 =parseunt(document.getElementById('l2').value);
+  document.write("annoit luvut: " + luk + " , "+ luku2 + " , "+ luku3 + "<be>");
+if (luku1 < luku2 && luku1 < luku3)
+{
+  if (luku2 < luku3)
   {
-    if(luku2 < luku3)
-    {
-      document.getElementById('t1').innerHtml = "luvut järstyksessä: " + luku2 " " + luku1 " " + kuku3 ;
-    }
-      else {
-        document.getElementById('t1').innerHtml = "luvut järstyksessä: " + luku1 " " + luku2 " " + kuku3 ;
-
-      }
-    }
-    else {
-      if (luku1 < luku2)
-      {
-        document.getElementById('t1').innerHtml = "luvut järstyksessä: " + luku3 " " + luku2 " " + kuku1 ;
-      }
-      else {
-        document.getElementById('t1').innerHtml = "luvut järstyksessä: " + luku2 " " + luku1 " " + kuku3 ;
-
-      }
-    }
-    else if (luku2 < luku1)
-    console.log(luku1, luku2, luku3)
+    document.write("Järjestys: " + luku1 + " , " + luku2 + " , " + luku3);
+  }
+  else
+  {
+    document.write("Järjestys: " + luku1 + " , " + luku3 + " , " + luku2);
   }
 }
-function kolme()
+else if (luku2 < luku1 && luku2 < luku3)
 {
-  var luku1, luku2, luku3, luku4, luku5;
-  luku1 = document. getElementById('l1').value;
-  luku2 = document. getElementById('l2').value;
-  luku3 = document. getElementById('l3').value;
-  luku4 = document. getElementById('l4').value;
-  luku5 = document. getElementById('l5').value;
-  /*document.getElementById('t1').innerHtml = luku.sort()*/
-  if (luku1 > luku2 && luku1 > luku3 && luku1 > luku4 && luku1 > luku5)
+  if (luku1 < luku3)
   {
-    if(luku2 > luku3)
-    {
-      document.getElementById('t2').innerHtml = "luvut järstyksessä: " + luku2 " " + luku1 " " + luku3 + " "  + luku4 " " + luku5 ;
-    }
-    else {
-      document.getElementById('t2').innerHtml = "luvut järstyksessä: " + luku1 " " + luku2 " " + luku3 + " "  + luku4 " " + luku5 ;
-
-    }
+    document.write("Järjestys: " + luku2 + " , " + luku1 + " , " + luku3);
   }
-  else {
-    if (luku1 > luku2 )
-    {
-      document.getElementById('t2').innerHtml = "luvut järstyksessä: " + luku3 " " + luku2 " " + luku1 + " "  + luku4 " " + luku5 ;
-    }
-    else {
-      document.getElementById('t2').innerHtml = "luvut järstyksessä: " + luku2 " " + luku1 " " + luku3 + " "  + luku4 " " + luku5 ;
-    }
-  }
-  if (luku3 > luku4)
-  {
-    document.getElementById('t2').innerHtml = "luvut järstyksessä: " + luku4 " " + luku2 " " + luku3 + " "  + luku1 " " + luku5 ;
-  }
-  else {
-    document.getElementById('t2').innerHtml = "luvut järstyksessä: " + luku1 " " + luku2 " " + luku3 + " "  + luku4 " " + luku5 ;
-  }
-  if (luku4 > luku5)
-  {
-    document.getElementById('t2').innerHtml = "luvut järstyksessä: " + luku5 " " + luku2 " " + luku3 + " "  + luku4 " " + luku1 ;
-  }
-  else {
-    document.getElementById('t2').innerHtml = "luvut järstyksessä: " + luku1 " " + luku2 " " + luku3 + " "  + luku4 " " + luku5 ;
-  }
-  else if (luku1 > luku2)
-    console.log(luku1, luku2, luku3, luku5);
-  }
-}
-function kolme()
+  else
+   {
+     document.write("Järjestys: " + luku2 + " , " + luku3 + " , " + luku1);
+   }
+ }
+ else if (luku3 < luku1 && luku3 < luku2)
  {
-  var luku
+  if (luku1 < luku2)
+  {
+    document.write("Järjestys: " + luku3 + " , " + luku1 + " , " + luku2);
+  }
+  else
+  {
+    document.write("Järjestys: " + luku3 + " , " + luku2 + " , " + luku1);
+  }
+ }
+}
+function etsi()
+{
+var l1  = parseunt(document.getElementById('luku1').value);
+var l2  = parseunt(document.getElementById('luku2').value);
+var l3  = parseunt(document.getElementById('luku3').value);
+var l4  = parseunt(document.getElementById('luku4').value);
+var l5  = parseunt(document.getElementById('luku5').value);
+document.write ("Annoit luvut: " + l1 + ", " + l2 + ", " + l3 + ", " + l4 + ", " + l5 + "<br>");
+document.write("Suurin luku on: ")
+document.write(Math.max(l1, l2, l3, l4, l5));
+}
 
+function tarkista()
+{
+  var tulos = parseunt(document.getElementById('8').value);
+
+  if (tulos%2 == 0)
+  {
+    alert("numero" + tulos + " on parillinen")
+  }
+  else
+  {
+    alert("Numero " + tulos + " ei ole parillinen")
+  }
+}
+
+function näytä()
+{
+  var ajaoneuvo = parseunt(document.getElementById('9').value);
+  if (ajoneuvo < 15)
+  {
+    alert("Saat ajaa polkupyörällä");
+  }
+  else if (ajoneuvo < 18)
+  {
+        alert("Saat ajaa mopolla");
+  }
+  else if (ajoneuvo < 100)
+  {
+      alert("saat ajaa autolla")
+  }
+}
+
+function kaanna()
+{
+  var kieli = document.getElementById('seitseman').value);
+  if (kieli == 'eng')
+  {
+    alert("Hello world");
+  }
+  else if (kieli == 'esp')
+  {
+    alert("Hola mundo");
+  }
+  else if (kieli == 'swe')
+  {
+    alert("Hej världen");
+  }
 }
