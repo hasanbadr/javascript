90244973f1aa242ff6c87bf7100c4d18e6e038f8
hasanbadr <hasan.badr@edu.keuda.fi>
2022-11-23 11:29:47 +0200

    hasan

--- projektit/projekti 2/arvauspeli.js
+++ projektit/projekti 2/arvauspeli.js
@@ -1,38 +1,65 @@
-var highLimit = 10;
-   var randNum = Math.floor((Math.random() * highLimit) + 1);
-   var allowedGuesses = 3;
-   var numGuesses = 0;
-   function guessingGame() {
-       if (numGuesses <= allowedGuesses) {
-          do {
-               inputNum = document.numForm.number.value;
-               inputNum = parseInt(inputNum);
-               if (inputNum < randNum && inputNum > 1) {
-                   document.numForm.gameResults.value = "Sorry, your guess was too low.";
-                   numGuesses++;
-               }
-               else if (inputNum > randNum && inputNum < highLimit) {
-                   document.numForm.gameResults.value = "Sorry, your guess was too high.";
-                   numGuesses++;
-               }
-               else if (inputNum == randNum) {
-                   document.numForm.gameResults.value = "Congratulations! You guessed correctly.";
-                   numGuesses++;
-               }
-               else {
-                   document.numForm.gameResults.value = "Your guess was out of the desired parameters. Please guess again.";
-                   numGuesses--;
-               }
-           } while(numGuesses < allowedGuesses && inputNum != randNum);
-       }
-           else {
-               document.numForm.gameResults.value = "Sorry, you have reached the allowed number of guesses, which is " + allowedGuesses + "." + " Click 'New Game' to try again.";
-           }
-       return false;
-   }
-   numGuesses = 0;
-   function newGame() {
-           randNum = Math.floor((Math.random() * highLimit) + 1);
-           guessingGame();
-           return false;
-      }
+//Määritetään muuttujat ja haetaan html-documentista käyttöjän syöttämä luku
+var luku1 = document.getElementById('message1');
+var luku2 = document.getElementById('message2');
+var luku3 = document.getElementById('message3');
+var clicks = 0;
+var answer = math.floor(math.random()*10)+1;
+var nro_of_guesses =  1;
+var guessed_nums = [];
+var wins = 0;
+var loose = 0;
+function arvauspeli()
+{
+  var user_guess = document.getElementById('guess').value;
+  if (user_guess >=10) {
+    guessed_nums.push(user_guess);
+    message2.innerHTML = "nro_of_guesses:" + nro_of_guesses;
+    message2.innerHTML = "guessed numbers:" + guessed_nums;
+  }
+  else
+  {
+    if (user_guess < 1 || user_guess > 10)
+    {
+      alert("ole hyvä ja anna numero 1 ja 10 välillä")
+    }
+    else
+    {
+      guessed_nums.push(user_guess);
+    }
+    if (user_guess > answer)
+    {
+      message1.innerHTML = "is too low";
+    }
+    else if (user_guess > answer)
+    {
+      message1.innerHTML = "is too high";
+    }
+    else if (user_guess > answer)
+    {
+      message1.innerHTML = "nice! you win!";
+      wins++;
+      document.getElementById("btn").hidden = true;
+     }
+     else
+     {
+      alert("anna numeroina, ei kirjaimina")
+     }
+  }
+}
+//Kun painetaan "uusipeli" web-sivustossa, niin sivu tyhjentää käyttäjän laittaman numeron
+function uusipeli()
+{
+  document.getElementById('formi').reset();
+}
+answer = math.floor(Math.random()*10)+1;
+//Annetaan käyttäjälle kolme arvaus kertaa
+function onClick()
+{
+  if (clicks >= 3)
+    return;
+    clicks += 1;
+    document.getElementById('loose').innerHTML = "Looses" + Looses;
+    document.getElementById('wins').innerHTML = "wins" + Wins;
+    document.getElementById('clicks'),innerHTML = "clicks" + Clicks;
+    nor_of_guesses++;
+  };
